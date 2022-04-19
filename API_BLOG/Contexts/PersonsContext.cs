cfb5a95f6cf357c601e2ef0d28ea560e2126cd3f
AnasEfi <70594830+AnasEfi@users.noreply.github.com>
2022-04-20 00:30:04 +0300

    addContext

---
+++ API_BLOG/Contexts/PersonsContext.cs
@@ -0,0 +1,31 @@
+﻿using API_BLOG.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_BLOG.Contexts
+{
+    public class PersonsContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Achievment> Achievments { get; set; }
+        public PersonsContext(DbContextOptions<PersonsContext> opts) : base(opts)
+
+        {
+            Database.EnsureCreated();
+
+            //    if (Persons != null)
+            //    {
+            //        foreach (var p in Persons)
+            //        {
+            //            if (p.Achievments != null && p.Achievments.Length > 0)
+            //                p.UpdateAchivmentsListFromJson();
+            //        }
+            //    }
+            //}
+
+
+
+
+        }
+    }
+}
---
+++ API_BLOG/Controllers/PersonController.cs
@@ -0,0 +1,131 @@
+﻿using API_BLOG.Contexts;
+using API_BLOG.Models;
+using API_BLOG.Models.BodyModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_BLOG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        PersonsContext PersonsDatabase;
+
+        public PersonController(PersonsContext personsContext)
+        {
+            PersonsDatabase = personsContext;
+
+        }
+
+        #region UpdatePersonalData: ChangePassword, UpdatePersonalData
+        // change password
+        // update other data
+        [Route("ChangePassword")]
+        [HttpPut]
+        [Authorize(Roles = "admin,user")]
+        public IActionResult ChangePassword([FromBody] ChangePasswordData changePasswordData)
+        {
+            if (Request.Cookies.TryGetValue("ID", out string? uid) == false)
+                return Unauthorized(new { error = "Cookie not found error. Authorize first" });
+
+            var person = PersonsDatabase.Persons.Where(p => p.Id == Convert.ToInt32(uid)).FirstOrDefault();
+            if (person == null)
+                return BadRequest(new { error = $"Unknown error. Cannot find person with your cookie id: '{uid}'" });
+
+            if (person.ComparePassword(changePasswordData.OldPassword) == false)
+                return BadRequest(new { error = "old password was incorrect" });
+
+            person.SetupNewPassword(changePasswordData.NewPassword);
+            PersonsDatabase.Update(person);
+            PersonsDatabase.SaveChanges();
+            return Ok(new
+            {
+                message = "Your password sucessfully changed!",
+                passwordHash = person.Password
+            });
+        }
+        [Route("UpdatePersonalData")]
+        [HttpPut]
+        [Authorize(Roles = "admin,user")]
+        public IActionResult UpdatePersonalData([FromBody] UpdateProfileData updateProfileData)
+        {
+            if (Request.Cookies.TryGetValue("ID", out string? uid) == false)
+                return Unauthorized(new { error = "Cookie not found error. Authorize first" });
+
+            var person = PersonsDatabase.Persons.Where(p => p.Id == Convert.ToInt32(uid)).FirstOrDefault();
+            if (person == null)
+                return BadRequest(new { error = $"Unknown error. Cannot find person with your cookie id: '{uid}'" });
+
+            person.UpdatePersonalData(updateProfileData);
+            PersonsDatabase.Update(person);
+            PersonsDatabase.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Your personal data successfully updated",
+                personalData = new
+                {
+                    email = person.Email,
+                    country = person.Country,
+                    profilePhoto = person.ProfilePhoto
+                }
+            });
+        }
+        #endregion
+
+        //CreateArticle, ShowAllArticles
+        [Route("CreateArticle")]
+        [HttpPost]
+        [Authorize(Roles = "admin, user")]
+        public IActionResult CreateArticle([FromBody] ArticleUpdate articleUpdateData)
+        {
+            // проверить куки
+            // получить пользователя
+
+            if (Request.Cookies.TryGetValue("ID", out string? uid) == false)
+                return BadRequest(new { error = "Cookie not found error. Authorize first" });
+
+            int _uid = Convert.ToInt32(uid);
+
+            var person = PersonsDatabase.Persons.Where(p => p.Id == _uid).FirstOrDefault();
+            if (person == null)
+                return BadRequest(new { error = $"Unknown error. Cannot find person with your cookie id: '{uid}'" });
+
+            if (articleUpdateData.Title == null || articleUpdateData.Content == null)
+                return BadRequest(new { error = "Title and Content fields required to be" });
+
+            var article = Article.Create(_uid, articleUpdateData);
+            PersonsDatabase.Articles.Add(article);
+            PersonsDatabase.SaveChanges();
+            return Ok(new
+            {
+                message = "Article successfully added!",
+                articleData = article
+            });
+        }
+        [Route("GetArticlesForUser")]
+        [HttpGet]
+        [Authorize(Roles = "admin,user")]
+        public IActionResult GetArticlesForUser()
+        {
+            // проверить куки -> взять id
+            // проверить что пользователь с таким id есть
+            // показать статьи
+
+            if (!Request.Cookies.TryGetValue("ID", out string? uid))
+                return BadRequest(new { error = "Cookie not found error. Authorize first" });
+
+            var person = PersonsDatabase.Persons.Where(p => p.Id == Convert.ToInt32(uid)).FirstOrDefault();
+            if (person == null)
+                return BadRequest(new { error = $"Unknown error. Cannot find person with your cookie id: '{uid}'" });
+
+            return Ok(new
+            {
+                message = "Request succeed",
+                personArticles = PersonsDatabase.Articles.Where(a => a.PersonId == Convert.ToInt32(uid)).ToList()
+            });
+        }
+    }
+}
